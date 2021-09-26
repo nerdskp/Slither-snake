1fc859d6e2e60d8e107059a4ce59befe29ec8d51
Surya Kiran Pramanik <91407328+nerdskp@users.noreply.github.com>
2021-09-26 13:02:32 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -15,7 +15,7 @@ dis_width = 600
 dis_height = 400
 
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Surya')
+pygame.display.set_caption('slither.snake')
 
 clock = pygame.time.Clock()
 
